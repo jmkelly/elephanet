539d6585dba94de83d08ebb878f9409de68b3dd2
James Kelly <james.kelly.99@gmail.com>
2015-11-09 20:26:35 +1100

    create schema generator for tidying up the DocumentSession

---
+++ Elephanet/Schema/ISchemaGenerator.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public interface ISchemaGenerator
+{
+    void GetOrCreateTable(Type type);
+}
---
+++ Elephanet/Schema/SchemaGenerator.cs
@@ -0,0 +1,89 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+
+namespace Elephanet
+{
+    
+
+    public class SchemaGenerator : ISchemaGenerator
+    {
+        NpgsqlConnection _connection;
+        IStoreInfo _storeInfo;
+        ITableInfo _tableInfo;
+
+        public SchemaGenerator(IDocumentStore store, NpgsqlConnection connection)
+        {
+            _connection = connection; 
+            _storeInfo = store.StoreInfo;
+            _tableInfo = store.Conventions.TableInfo;
+            _connection = connection;
+        }
+
+        bool IndexDoesNotExist(Type type)
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandText = string.Format(@"select count(*)
+                from pg_indexes
+                where schemaname = '{0}'
+                and tablename = '{1}'
+                and indexname = 'idx_{1}_body';", _tableInfo.Schema, _tableInfo.TableNameWithoutSchema(type));
+                var indexCount = (Int64)command.ExecuteScalar();
+                return indexCount == 0;
+            }
+
+        }
+
+        void CreateIndex(Type type)
+        {
+            if (IndexDoesNotExist(type))
+            {
+                using (var command = _connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = string.Format(@"CREATE INDEX idx_{0}_body ON {0} USING gin (body);", _tableInfo.TableNameWithoutSchema(type));
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void GetOrCreateTable(Type type)
+        {
+           
+            if (!_storeInfo.Tables.Contains(_tableInfo.TableNameWithSchema(type)))
+            {
+                _storeInfo.Tables.Add(_tableInfo.TableNameWithSchema(type));
+                try
+                {
+                    using (var command = _connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.Text;
+                        command.CommandText = string.Format(@"
+                            CREATE TABLE IF NOT EXISTS {0}
+                            (
+                                id uuid NOT NULL, 
+                                body jsonb NOT NULL, 
+                                created timestamp without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+                                CONSTRAINT pk_{1} PRIMARY KEY (id)
+                            );", _tableInfo.TableNameWithSchema(type), _tableInfo.TableNameWithoutSchema(type));
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(string.Format("Could not create table {0}; see the inner exception for more information.", _tableInfo.TableNameWithSchema(type)), exception);
+                }
+                try
+                {
+                    CreateIndex(type);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(string.Format("Could not create index on table {0}; see the inner exception for more information.", _tableInfo.TableNameWithSchema(type)), exception);
+                }
+            }
+        }
+    }
+}
