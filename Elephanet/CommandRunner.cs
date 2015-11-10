a05e59ed69ee19a6baa0435670f50b37d6871c0e
James Kelly <james.kelly.99@gmail.com>
2015-11-10 21:37:32 +1100

    extract entity map to own class

--- Elephanet/CommandRunner.cs
+++ Elephanet/CommandRunner.cs
@@ -109,18 +109,5 @@ namespace Elephanet
         }
     }
 
-    public class EntityMap
-    {
-
-        public EntityMap(Type type, string tableName, string temporaryTableName)
-        {
-            EntityType = type;
-            TableName = tableName;
-            TemporaryTableName = temporaryTableName;
-        }
-
-        public Type EntityType { get; set; }
-        public string TableName { get; set; }
-        public string TemporaryTableName { get; set; }
-    }
+    
 }
---
+++ Elephanet/EntityMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class EntityMap
+{
+
+    public EntityMap(Type type, string tableName, string temporaryTableName)
+    {
+        EntityType = type;
+        TableName = tableName;
+        TemporaryTableName = temporaryTableName;
+    }
+
+    public Type EntityType { get; set; }
+    public string TableName { get; set; }
+    public string TemporaryTableName { get; set; }
+}
