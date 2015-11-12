dda1554768a96509f73e4545fc9220a5cdeb93f2
James Kelly <james.kelly.99@gmail.com>
2015-11-12 22:14:34 +1100

    add a batching class

---
+++ Elephanet.Tests/BatchCommanderTests.cs
@@ -0,0 +1,52 @@
+﻿using Elephanet.Tests.Entities;
+using Ploeh.AutoFixture;
+using Shouldly;
+using System.Linq;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Elephanet.Tests
+{
+    public class BatchCommanderTests
+    {
+        int batchSize;
+        List<object> updates;
+
+        public BatchCommanderTests()
+        {
+            updates = new Fixture().Build<EntityForBatchingTests>()
+                .CreateMany(10000).ToList<object>();
+            batchSize = 1000;
+        }
+
+        [Fact]
+        public void BatchSize_Should_BeSetCorrectly()
+        {
+            var commander = new Commander(updates, batchSize );
+            commander.BatchSize.ShouldBe(1000);
+        }
+
+        [Fact]
+        public void CommandBatch_Should_ReturnAListOfCommandsOfBatchSize()
+        {
+            var commander = new Commander(updates, batchSize );
+            var commands = commander.Batch();
+            commands[0].ShouldBeOfType<BatchedEntities>();
+            commands[0].Entities.Count().ShouldBe(batchSize);
+            commands[1].Entities.Count().ShouldBe(batchSize);
+            commands[2].Entities.Count().ShouldBe(batchSize);
+        }
+
+        [Fact]
+        public void CommandBatch_Should_FillIfLessThanBatchSize()
+        {
+            updates = new Fixture().Build<EntityForBatchingTests>()
+             .CreateMany(2).ToList<object>();
+
+            var commander = new Commander(updates, 1000);
+            var commands = commander.Batch();
+            commands[0].ShouldBeOfType<BatchedEntities>();
+            commands[0].Entities.Count().ShouldBe(2);
+        }
+    }
+}
---
+++ Elephanet/Commander.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Elephanet
+{
+    public class Commander
+    {
+        IList<object> _updates;
+        readonly int _batchSize;
+        private ISchemaGenerator _schemaGenerator;
+
+        public Commander(IList<object> updates, int batchSize = 1000)
+        {
+            _updates = updates;
+            _batchSize = batchSize;
+        }
+
+        public int BatchSize
+        {
+            get
+            {
+                return _batchSize;
+            }
+        }
+
+        public IList<BatchedEntities> Batch()
+        {
+            List<BatchedEntities> batches = new List<BatchedEntities>();
+            List<object> batch = new List<object>();
+            for (int i = 0; i < _updates.Count; i++)
+            {
+                batch.Add(_updates[i]);
+                int batchCheck = i + 1;
+                if (batchCheck % (_batchSize) == 0)
+                {
+                    batches.Add(new BatchedEntities(batch));
+                    batch = new List<object>();
+                }
+            }
+
+            //add the last batch on the end
+            batches.Add(new BatchedEntities(batch));
+            return batches;
+        }
+    }
+
+    public class BatchedEntities
+    {
+        public BatchedEntities(List<object> entities)
+        {
+            Entities = entities;
+        }
+        public List<object> Entities { get; set; }
+    }
+}
