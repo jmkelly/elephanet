8a84bda2e0bfbee65675bca7b3c705fa86f8d427
James Kelly <james.kelly.99@gmail.com>
2015-11-12 22:15:54 +1100

    remove schema generation stuff, and include new batching support

--- Elephanet/CommandRunner.cs
+++ Elephanet/CommandRunner.cs
@@ -42,69 +42,60 @@ namespace Elephanet
             _jsonConverter = jsonConverter;
         }
 
-        HashSet<EntityMap> MatchEntityToFinalTableAndTemporaryTable(Dictionary<Guid, object> entities)
-        {
-            var typeToTableMap = new HashSet<EntityMap>();
-
-            var types = entities.Values.Select(v => v.GetType()).Distinct();
-            foreach (Type type in types)
-            {
-                typeToTableMap.Add(new EntityMap(type, _tableInfo.TableNameWithSchema(type), Guid.NewGuid().ToString()));
-            }
-
-            return typeToTableMap;
-        }
+       
 
         public void Execute()
         {
 
             var sb = new StringBuilder();
 
-            HashSet<EntityMap> matches = MatchEntityToFinalTableAndTemporaryTable(_entities);
-
+            HashSet<EntityMap> matches = _schemaGenerator.MatchEntityToFinalTableAndTemporaryTable(_entities);
+            List<object> updates = new List<object>();
 
             foreach (var item in _entities)
             {
                 //make sure we have tables for all types
                 _schemaGenerator.GetOrCreateTable(item.Value.GetType());
+                updates.Add(item.Value);
             }
 
-            StringBuilder createTempTable = new StringBuilder();
-            StringBuilder dropTempTable = new StringBuilder();
+            var commander = new Commander(updates, 5000);
 
             sb.Append("BEGIN;");
-            List<string> temporaryTableName = new List<string>();
-            foreach (var match in matches)
-            {
-                createTempTable.Append(string.Format("CREATE TABLE {0} (id uuid, body jsonb);", match.TemporaryTableName.SurroundWithDoubleQuotes()));
-                dropTempTable.Append(string.Format("DROP TABLE {0};", match.TemporaryTableName.SurroundWithDoubleQuotes()));
-            }
-
-            foreach (var item in _entities)
-            {
-                sb.Append(string.Format("INSERT INTO {0} (id, body) VALUES ('{1}', '{2}');", matches.Where(c => c.EntityType == item.Value.GetType()).Select(j => j.TemporaryTableName).First().SurroundWithDoubleQuotes(), item.Key, _jsonConverter.Serialize(item.Value).EscapeQuotes()));
-            }
 
             foreach (var match in matches)
             {
-                sb.Append(string.Format("LOCK TABLE {0} IN EXCLUSIVE MODE;", match.TableName));
-                sb.Append(string.Format("UPDATE {0} SET body = tmp.body from {1} tmp where tmp.id = {0}.id;", match.TableName, match.TemporaryTableName.SurroundWithDoubleQuotes()));
-                sb.Append(string.Format("INSERT INTO {0} SELECT tmp.id, tmp.body from {1} tmp LEFT OUTER JOIN {0} ON ({0}.id = tmp.id) where {0}.id IS NULL;", match.TableName, match.TemporaryTableName.SurroundWithDoubleQuotes()));
+                sb.Append(string.Format("CREATE TEMPORARY TABLE IF NOT EXISTS {0} (id uuid, body jsonb);", match.TemporaryTableName.SurroundWithDoubleQuotes()));
             }
 
 
-            sb.Append("COMMIT;");
-
-            using (var command = _connection.CreateCommand())
+            foreach (var batch in commander.Batch())
             {
-                command.CommandTimeout = 60;
-                command.CommandType = CommandType.Text;
-                command.CommandText = createTempTable.ToString();
-                command.ExecuteNonQuery();
-                command.CommandText = sb.ToString();
-                command.ExecuteNonQuery();
-                command.CommandText = dropTempTable.ToString();
-                command.ExecuteNonQuery();
+                List<string> temporaryTableName = new List<string>();
+               
+                foreach (var item in batch.Entities)
+                {
+                    var IdProperty = "Id";
+                    var propertyInfo = item.GetType().GetProperty(IdProperty);
+                    var id = propertyInfo.GetValue(item, null);
+                    sb.Append(string.Format("INSERT INTO {0} (id, body) VALUES ('{1}', '{2}');", matches.Where(c => c.EntityType == item.GetType()).Select(j => j.TemporaryTableName).First().SurroundWithDoubleQuotes(), id, _jsonConverter.Serialize(item).EscapeQuotes()));
+                }
+
+                foreach (var match in matches)
+                {
+                    sb.Append(string.Format("LOCK TABLE {0} IN EXCLUSIVE MODE;", match.TableName));
+                    sb.Append(string.Format("UPDATE {0} SET body = tmp.body from {1} tmp where tmp.id = {0}.id;", match.TableName, match.TemporaryTableName.SurroundWithDoubleQuotes()));
+                    sb.Append(string.Format("INSERT INTO {0} SELECT tmp.id, tmp.body from {1} tmp LEFT OUTER JOIN {0} ON ({0}.id = tmp.id) where {0}.id IS NULL;", match.TableName, match.TemporaryTableName.SurroundWithDoubleQuotes()));
+                }
+
+                sb.Append("COMMIT;");
+
+                using (var command = _connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = sb.ToString(); 
+                    command.ExecuteNonQuery();
+                }
             }
         }
     }
